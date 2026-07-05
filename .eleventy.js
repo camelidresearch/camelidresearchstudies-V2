7a35de0d8c871345289c7b716841c337d40334e4
Pietro <pietrociaschi@gmail.com>
2026-07-05 13:02:22 +0200

    Ottimizzazione automatica immagini (elevently-img-transform))

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
   // Copia gli asset statici (CSS, immagini) così come sono
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
@@ -5,6 +7,20 @@ module.exports = function (eleventyConfig) {
 
   // Anno corrente, comodo per il footer
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Ottimizzazione automatica di tutte le <img> in build
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    extensions: "html",
+    formats: ["avif", "webp", "auto"],
+    widths: [400, 800, 1200, "auto"],
+    outputDir: "./_site/assets/img/optimized/",
+    urlPath: "/assets/img/optimized/",
+    defaultAttributes: {
+      loading: "lazy",
+      decoding: "async",
+      sizes: "(min-width: 768px) 50vw, 100vw"
+    }
+  });
 
   return {
     dir: {
